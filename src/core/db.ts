59b1f64ddf200c3fec7808552c1d7ba3a718e238
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-02 17:46:17 +0330

    feat: :sparkles: Ability to sync selected words with IndexedDB and store them

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,17 +1,39 @@
+import { UserSelectedWords } from "@/types/user-selected-words.interface";
 import Dexie, { type EntityTable } from "dexie";
 
-interface UserWordSelected {
-  wid: number;
-  text: string;
+interface WordsSelected extends UserSelectedWords {
+	id: number;
+	sentense: string;
+	start_time: number;
+	end_time: number;
+	episode_id: string;
+	podcast_id: string;
+	has_update: Date;
 }
 
-const db = new Dexie("UserWordSelected") as Dexie & {
-  userWordSelected: EntityTable<UserWordSelected, "wid">;
+const db = new Dexie("syncDB") as Dexie & {
+	userWordSelected: EntityTable<WordsSelected, "id">;
 };
 
 db.version(1).stores({
-  userWorSelected: "++wid, text", // primary key "id" (for the runtime!)
+	userWordSelected: "++id,wid, text", // primary key "id" (for the runtime!)
 });
 
-export type { UserWordSelected };
+export const getWordById = async (wid: number) => {
+	const word = await db.userWordSelected.where("wid").equals(wid);
+	return word;
+};
+
+export const addOrUpdateWordById = async (wid: number, text: string) => {
+	try {
+		return await db.userWordSelected.add({ wid: wid, text: text });
+	} catch (error) {
+		console.log("error :: ", error);
+	}
+};
+
+export const deleteWordById = async (wid: number) => {
+	await db.userWordSelected.delete(wid);
+};
+
 export { db };
