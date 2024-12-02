3b2d1646a0ff282b79504e7641e8b5b0180d6c6e
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-02 17:21:31 +0330

    refactor(types): :recycle: add more types for working transcript

--- src/types/episode.interface.ts
+++ src/types/episode.interface.ts
@@ -1,16 +1,31 @@
-export interface TranscriptWord {
-  wid?: number;
-  text: string;
+export interface SentenseWords {
+  word: Words;
+  text?: string;
+  phrase?: Phrasal;
 }
-
-export interface TranscriptJsonb {
+export interface Sentense {
   sentense_id: number;
-  TranscriptWord: TranscriptWord[];
+  sentense_words: SentenseWords[];
   start_time_sec: number;
   end_time_sec: number;
 }
 
 export interface EpisodeData {
   audio_url: string;
-  transcript_timing: TranscriptJsonb;
+  transcript_timing: Sentense[];
 }
+
+export type Words = {
+  wid?: number;
+  text: string;
+};
+
+export type PhraseSentense = {
+  word: Words;
+  text?: string;
+};
+
+export type Phrasal = {
+  wid: number;
+  content: PhraseSentense[];
+};
