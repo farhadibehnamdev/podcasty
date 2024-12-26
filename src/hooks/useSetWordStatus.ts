01cd1375f8b315f00cb6c428f3ae07d9e520b92e
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-26 16:15:32 +0330

    feat: add setWordStatus mutation for updating word status
    - Added `setWordStatus` function to handle updating word status via API.
    - Defined `SetWordStatusParams` and `UserWordType` interfaces for type safety.
    - Utilized `useMutation` from `@tanstack/react-query` for mutation handling.
    - Configured API headers and endpoint URL.

---
+++ src/hooks/useSetWordStatus.ts
@@ -0,0 +1,46 @@
+import { API_KEY, API_LOCAL, API_PRODUCTION_EDGE } from "@/configs/globals";
+import { createData } from "@/core/http-service";
+import { UserWords } from "@/core/models/user-words";
+import { WordsSelected } from "@/types/words-selected.interface";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  AxiosHeaders,
+  RawAxiosRequestHeaders,
+  RawAxiosResponseHeaders,
+} from "axios";
+
+interface SetWordStatusParams {
+  id: number;
+  userWord: WordsSelected;
+}
+interface UserWordType {
+  // Renamed from UserWordsType for consistency
+  userWord: WordsSelected;
+}
+
+const setWordStatus = async ({ id, userWord }: SetWordStatusParams) => {
+  const url = `${API_PRODUCTION_EDGE}/word-status/${id}`;
+  const headers: RawAxiosRequestHeaders = {
+    Authorization: `Bearer ${API_KEY}`,
+    apiKey: API_KEY,
+  };
+
+  const response = await createData<UserWordType, RawAxiosResponseHeaders>(
+    url,
+    {
+      userWord: {
+        ...userWord,
+        status: "unknown",
+        user_id: "1a20da04-4a6a-495c-bffc-353e806d30a8",
+      },
+    },
+    headers
+  );
+  return response;
+};
+
+export const useSetWordStatus = () => {
+  return useMutation({
+    mutationFn: (params: SetWordStatusParams) => setWordStatus(params),
+  });
+};
