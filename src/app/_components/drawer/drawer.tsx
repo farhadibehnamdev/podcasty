999a73e1929da3aa74d9c9fccba429a51aa45a66
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-26 16:16:17 +0330

    feat: add initial Drawer component with context and UI elements
    - Implemented Drawer component using `@nextui-org/react` components.
    - Integrated `DrawerContext` for managing drawer state.
    - Added icons from `lucide-react` for visual elements.
    - Defined `ResponseMutationWordExplanation` interface for type safety.

--- src/app/_components/drawer/drawer.tsx
+++ src/app/_components/drawer/drawer.tsx
@@ -1,209 +1,181 @@
 "use client";
-import React, { useContext } from "react";
-import { clsx } from "clsx";
+import React, { ReactNode, useContext, useEffect } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Chip,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  Drawer as DrawerNext,
+  Tab,
+  Tabs,
+} from "@nextui-org/react";
 import { DrawerContext } from "@/context";
-import { Checkbox, Chip, Divider, Switch } from "@nextui-org/react";
-import { ClipboardType, Settings2 } from "lucide-react";
+import { CircleCheck, Heart, Volume1 } from "lucide-react";
+import { useWordExplanationStore } from "@/stores/word-explanation.store";
+import { Mutation, useMutationState } from "@tanstack/react-query";
 
-export const Drawer = () => {
-  const { open, setOpen } = useContext(DrawerContext);
+interface ResponseMutationWordExplanation {
+  word: string;
+  level: string;
+  synonyms: string[];
+  examples: string[];
+  explanation: string;
+  translation: string;
+  phonetic: string;
+  audio: string;
+}
+
+export const Drawer: React.FC = () => {
+  const { isOpen, onOpenChange } = useContext(DrawerContext);
+
+  const data = useMutationState({
+    filters: { mutationKey: ["word-explain"] },
+    select: (mutation: Mutation) => mutation.state.data,
+  });
+  const latest = data[data.length - 1] as ResponseMutationWordExplanation;
+  console.log("latest :: ", latest?.word);
+  const playPronunce = () => {
+    const word = new Audio(
+      `https://dict.youdao.com/dictvoice?type=0&audio=${latest?.word}`
+    );
+    word.play();
+  };
   return (
-    <div
-      className="relative z-50 container"
-      aria-labelledby="slide-over-title"
-      role="dialog"
-      aria-modal="true"
-      onClick={() => setOpen(!open)}
+    <DrawerNext
+      className="rounded-none"
+      isOpen={isOpen}
+      placement="right"
+      onOpenChange={onOpenChange}
     >
-      {open ? (
-        <div
-          className="fixed inset-0 bg-gray-500/75 transition-opacity"
-          aria-hidden="true"
-        ></div>
-      ) : (
-        ""
-      )}
+      <DrawerContent>
+        <DrawerHeader className="flex flex-col gap-1">
+          <Chip size="lg">Details</Chip>
+        </DrawerHeader>
 
-      <div className={clsx({ "fixed inset-0 overflow-hidden": open })}>
-        <div className="absolute inset-0 overflow-hidden">
-          <div className="pointer-events-none fixed inset-y-0 left-0 flex">
-            <div
-              className={clsx(
-                "pointer-events-auto relative w-full h-full flex-col transform transition ease-in-out duration-500  bg-white py-6 shadow-xl",
-                { "translate-x-0": open },
-                { "-translate-x-full": !open }
-              )}
-              onClick={(event) => {
-                event.preventDefault();
-                event.stopPropagation();
-              }}
-            >
-              <div className="px-4 sm:px-6">
-                <h2
-                  className="text-base font-semibold text-gray-900 mb-5 content-center text-center container"
-                  id="slide-over-title"
+        <DrawerBody className="mt-10">
+          {/* <div className="flex justify-center gap-3 drop-shadow-md mb-6">
+            <Button color="danger">Unknown</Button>
+            <Button color="success">Mastered</Button>
+            <Button color="warning">Add To SRS</Button>
+          </div> */}
+          <div className="flex w-full flex-col">
+            <Tabs aria-label="Options" size="lg" fullWidth>
+              <Tab key="dictionary" title="Dictionary">
+                {!!latest?.word ? (
+                  <iframe
+                    src={`https://api.s1.zabanshenas.com/api/v1/dictionary?dictionary=cambridgeen&word=${latest?.word}`}
+                    width="100%"
+                    className="h-[80vh]"
+                  ></iframe>
+                ) : (
+                  "Loading..."
+                )}
+              </Tab>
+              <Tab key="ai" title="AI">
+                <Card
+                  className="rounded-md bg-zinc-200 ring-1 ring-zinc-300 mb-10"
+                  shadow="sm"
                 >
-                  <span className="text-center container flex justify-center items-center gap-2">
-                    <Chip className=" flex justify-center items-center">
-                      Settings
-                    </Chip>
-                  </span>
-                </h2>
-                <div className="grid grid-cols[1fr_1fr] container text-center  rounded-md shadow-sm  ring-1 ring-zinc-300 p-4">
-                  <h3 className="text-base font-semibold text-gray-900 mb-3 text-center flex justify-center">
-                    <span className="text-center flex justify-center items-center gap-2">
-                      <ClipboardType />
-                      Vocabulary Levels
-                    </span>
-                  </h3>
-                  <Divider orientation="horizontal" className=" mb-5" />
-                  <div className="grid grid-cols-[1fr_1fr_1fr] gap-2 mb-5">
-                    <Checkbox
-                      color="success"
-                      defaultSelected
-                      radius="lg"
-                      size="lg"
-                    >
-                      A1
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      size="lg"
-                      color="secondary"
-                      radius="md"
-                    >
-                      A2
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      size="lg"
-                      color="danger"
-                      radius="md"
-                    >
-                      B1
-                    </Checkbox>
-                  </div>
-                  <div className="grid grid-cols-[1fr_1fr_1fr] gap-2">
-                    <Checkbox
-                      defaultSelected
-                      size="lg"
-                      color="warning"
-                      radius="md"
-                    >
-                      B2
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      classNames={{ wrapper: "after:bg-orange-600" }}
-                      radius="md"
-                      size="lg"
-                    >
-                      C1
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      classNames={{ wrapper: "after:bg-cyan-600" }}
-                      radius="md"
-                      size="lg"
-                    >
-                      C2
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      classNames={{ wrapper: "after:bg-indigo-600" }}
-                      radius="md"
-                      size="lg"
-                    >
-                      All
-                    </Checkbox>
-                  </div>
-
-                  <Divider orientation="horizontal" className="my-4" />
-                  <Switch className="text-lg font-semibold">
-                    Phrasal Verbs
-                  </Switch>
+                  <CardBody>
+                    <div className="flex justify-between p-3">
+                      <div className="flex justify-between gap-3">
+                        <span className="font-semibold text-lg drop-shadow-sm">
+                          {latest?.word}
+                        </span>
+                        <span className="rounded-sm ring-1 ring-zinc-600 p-1 text-sm">
+                          US
+                        </span>
+                      </div>
+                      <div className="flex justify-start gap-3">
+                        <span>{latest?.phonetic}</span>
+                        <Volume1
+                          onClick={playPronunce}
+                          type="button"
+                          className="cursor-pointer"
+                        />
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+                <div className="flex w-full flex-col">
+                  <Tabs aria-label="Options">
+                    <Tab key="meaning" title="Meaning">
+                      <Card
+                        as="div"
+                        shadow="none"
+                        className="border-1 border-zinc-300"
+                      >
+                        <CardBody as="div">{latest?.explanation}</CardBody>
+                      </Card>
+                    </Tab>
+                    <Tab key="synonym" title="Synonym">
+                      <Card
+                        as="div"
+                        shadow="none"
+                        className="border-1 border-zinc-300"
+                      >
+                        <CardBody as="div">
+                          <ul>
+                            {latest?.synonyms?.map((syn, index) => {
+                              return (
+                                <li className="mb-3" key={syn}>
+                                  <span className="font-semibold">
+                                    {++index}.{" "}
+                                  </span>
+                                  <span className="font-sm">{syn}</span>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </CardBody>
+                      </Card>
+                    </Tab>
+                    <Tab key="examples" title="Example">
+                      <Card
+                        as="div"
+                        shadow="none"
+                        className="border-1 border-zinc-300"
+                      >
+                        <CardBody as="div">
+                          <ul>
+                            {latest?.examples?.map((ex, index) => {
+                              return (
+                                <li className="mb-3" key={index}>
+                                  <span className="font-semibold">
+                                    {++index}.{" "}
+                                  </span>
+                                  <span className="font-sm">{ex}</span>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        </CardBody>
+                      </Card>
+                    </Tab>
+                    <Tab key="translation" title="Translation">
+                      <Card
+                        as="div"
+                        shadow="none"
+                        className="border-1 border-zinc-300 text-center font-semibold "
+                      >
+                        <CardBody
+                          as="div"
+                          className="text-center font-bold text-lg"
+                        >
+                          <span>{latest?.translation}</span>
+                        </CardBody>
+                      </Card>
+                    </Tab>
+                  </Tabs>
                 </div>
-              </div>
-              <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                <div className="grid grid-cols[1fr_1fr] container text-center  rounded-md shadow-sm  ring-1 ring-zinc-300 p-4">
-                  <h3 className="text-base font-semibold text-gray-900 mb-3 text-center flex justify-center">
-                    <span className="text-center flex justify-center items-center gap-2">
-                      <Settings2 />
-                      Player Settings
-                    </span>
-                  </h3>
-                  <Divider orientation="horizontal" className=" mb-5" />
-                  <div className="grid grid-cols-[1fr_1fr_1fr] gap-2 mb-5">
-                    <Checkbox
-                      color="success"
-                      defaultSelected
-                      radius="lg"
-                      size="lg"
-                    >
-                      A1
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      size="lg"
-                      color="secondary"
-                      radius="md"
-                    >
-                      A2
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      size="lg"
-                      color="danger"
-                      radius="md"
-                    >
-                      B1
-                    </Checkbox>
-                  </div>
-                  <div className="grid grid-cols-[1fr_1fr_1fr] gap-2">
-                    <Checkbox
-                      defaultSelected
-                      size="lg"
-                      color="warning"
-                      radius="md"
-                    >
-                      B2
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      classNames={{ wrapper: "after:bg-orange-600" }}
-                      radius="md"
-                      size="lg"
-                    >
-                      C1
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      classNames={{ wrapper: "after:bg-cyan-600" }}
-                      radius="md"
-                      size="lg"
-                    >
-                      C2
-                    </Checkbox>
-                    <Checkbox
-                      defaultSelected
-                      classNames={{ wrapper: "after:bg-indigo-600" }}
-                      radius="md"
-                      size="lg"
-                    >
-                      All
-                    </Checkbox>
-                  </div>
-
-                  <Divider orientation="horizontal" className="my-4" />
-                  <Switch className="text-lg font-semibold">
-                    Phrasal Verbs
-                  </Switch>
-                </div>
-              </div>
-            </div>
+              </Tab>
+            </Tabs>
           </div>
-        </div>
-      </div>
-    </div>
+        </DrawerBody>
+      </DrawerContent>
+    </DrawerNext>
   );
 };
