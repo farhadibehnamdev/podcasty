38c3b32606cf96f1002531ee16d8fb26416dc891
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-26 18:05:20 +0330

    fix: remove unnecessary lines of code that it was testing purposes.

--- src/app/(content)/podcast/[id]/_components/podcast.tsx
+++ src/app/(content)/podcast/[id]/_components/podcast.tsx
@@ -1,16 +1,8 @@
 "use client";
 
 import { Player } from "@/app/_components/player";
-import { Drawer } from "@/app/_components/drawer";
 
 const Podcast = () => {
-  return (
-    <>
-      <Drawer>
-        <p>hi</p>
-      </Drawer>
-      <Player />
-    </>
-  );
+  return <Player />;
 };
 export default Podcast;
