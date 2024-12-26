3cb2df165c4cb12aea992fc9b2da57913bea1f36
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-26 19:40:27 +0330

    fix: remove unnecessary lines of code

--- src/app/_components/player/_components/menus.tsx
+++ src/app/_components/player/_components/menus.tsx
@@ -3,18 +3,10 @@ import type { ReactElement } from "react";
 import {
   Menu,
   Tooltip,
-  useCaptionOptions,
   type MenuPlacement,
   type TooltipPlacement,
 } from "@vidstack/react";
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  ClosedCaptionsIcon,
-  RadioButtonIcon,
-  RadioButtonSelectedIcon,
-  SettingsIcon,
-} from "@vidstack/react/icons";
+import { SettingsIcon } from "@vidstack/react/icons";
 
 import { buttonClass, tooltipClass } from "./buttons";
 
@@ -42,80 +34,15 @@ export function Settings({ placement, tooltipPlacement }: SettingsProps) {
           Settings
         </Tooltip.Content>
       </Tooltip.Root>
-      <Menu.Content className={menuClass} placement={placement}>
-        <CaptionSubmenu />
-      </Menu.Content>
-    </Menu.Root>
-  );
-}
-
-function CaptionSubmenu() {
-  const options = useCaptionOptions(),
-    hint = options.selectedTrack?.label ?? "Off";
-  return (
-    <Menu.Root>
-      <SubmenuButton
-        label="Captions"
-        hint={hint}
-        disabled={options.disabled}
-        icon={ClosedCaptionsIcon}
-      />
-      <Menu.Content className={submenuClass}>
-        <Menu.RadioGroup
-          className="w-full flex flex-col"
-          value={options.selectedValue}
-        >
-          {options.map(({ label, value, select }) => (
-            <Radio value={value} onSelect={select} key={value}>
-              {label}
-            </Radio>
-          ))}
-        </Menu.RadioGroup>
-      </Menu.Content>
     </Menu.Root>
   );
 }
 
 export interface RadioProps extends Menu.RadioProps {}
 
-function Radio({ children, ...props }: RadioProps) {
-  return (
-    <Menu.Radio
-      className="ring-media-focus group relative flex w-full cursor-pointer select-none items-center justify-start rounded-sm p-2.5 outline-none data-[hocus]:bg-white/10 data-[focus]:ring-[3px]"
-      {...props}
-    >
-      <RadioButtonIcon className="h-4 w-4 text-white group-data-[checked]:hidden" />
-      <RadioButtonSelectedIcon className="text-media-brand hidden h-4 w-4 group-data-[checked]:block" />
-      <span className="ml-2">{children}</span>
-    </Menu.Radio>
-  );
-}
-
 export interface SubmenuButtonProps {
   label: string;
   hint: string;
   disabled?: boolean;
   icon: ReactElement;
-}
-
-function SubmenuButton({
-  label,
-  hint,
-  icon: Icon,
-  disabled,
-}: SubmenuButtonProps) {
-  return (
-    <Menu.Button
-      className="ring-media-focus parent left-0 z-10 flex w-full cursor-pointer select-none items-center justify-start rounded-sm bg-black/60 p-2.5 outline-none ring-inset data-[open]:sticky data-[open]:-top-2.5 data-[hocus]:bg-white/10 data-[focus]:ring-[3px] aria-disabled:hidden"
-      disabled={disabled}
-    >
-      <ChevronLeftIcon className="parent-data-[open]:block -ml-0.5 mr-1.5 hidden h-[18px] w-[18px]" />
-      <div className="contents parent-data-[open]:hidden">
-        <Icon className="w-5 h-5" />
-      </div>
-      <span className="ml-1.5 parent-data-[open]:ml-0">{label}</span>
-      <span className="ml-auto text-sm text-white/50">{hint}</span>
-      <ChevronRightIcon className="parent-data-[open]:hidden ml-0.5 h-[18px] w-[18px] text-sm text-white/50" />
-    </Menu.Button>
-  );
 }
