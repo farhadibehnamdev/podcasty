f6bd21318cdba8fe64dd0db7a5cbd135f26ef38f
farhadibehnamdev <behnammfarhadi@gmail.com>
2024-12-27 11:45:18 +0330

    feat: add NEXT_PUBLIC_API_URL to globals.ts
    - Added NEXT_PUBLIC_API_URL to the global configuration file.

--- src/configs/globals.ts
+++ src/configs/globals.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL;
-export const API_KEY = process.env.API_KEY;
-export const API_LOCAL = process.env.API_LOCAL_URL;
-export const API_PRODUCTION_EDGE = process.env.API_PRODUCTION_URL;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export const API_LOCAL = process.env.NEXT_PUBLIC_API_LOCAL_URL;
+export const API_PRODUCTION_EDGE = process.env.NEXT_PUBLIC_API_PRODUCTION_URL;
